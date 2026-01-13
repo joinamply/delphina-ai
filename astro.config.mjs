10366bb6599572bc794c5615ce1a2d3e094b4977
Alberto de Olivira <betodeoliveira@outlook.com>
2026-01-13 17:49:12 -0300

    Configure vite server to allow Netlify preview domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      host: true,
+      allowedHosts: [
+        'devserver-preview--delphina-ai.netlify.app'
+      ]
+    }
+  }
+});
+
